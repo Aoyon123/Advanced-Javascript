aa01e2f7e5e5813e39c7d0f3db2991929fc01759
Aoyon123 <49103044+Aoyon123@users.noreply.github.com>
2021-03-02 01:34:49 +0600

    Null Vs Undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+//let pakhi;
+//console.log(pakhi);
+
+//function add(num1,num2)
+//{
+ //   console.log(num2);
+
+//}
+
+
+
+//const personal={name:"Smart Dude",Phone:445611}
+
+
+//console.log(personal.address);
+
+let fun=undefined;
+console.log(fun);
+
