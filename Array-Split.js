fc5f670b043f676346817b033e4e92a5f8ad9a02
Aoyon123 <49103044+Aoyon123@users.noreply.github.com>
2021-03-03 01:43:51 +0600

    Array-Split

---
+++ Array-Split.js
@@ -0,0 +1,9 @@
+const nums=[1,2,3,4,5,6,7];
+//const part=nums.slice(2,5);
+
+const removed=nums.splice(2,3,44,55,66);
+console.log(removed)
+console.log(nums)
+
+const together=nums.join("Aoyon")
+console.log(together);
